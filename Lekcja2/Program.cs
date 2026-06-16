af74611b3c7aeb2541cd1caac8574d1e42a7b5ec
LukaszWalancik <97048575+LucasWalancik@users.noreply.github.com>
2026-06-16 10:16:04 +0200

    Add files via upload

---
+++ Lekcja2/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Lekcja2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Lekcja 2.    Zmienne     16.06.2026
+ 
+        // Część 1. Team psy, czy team koty?
+
+        // Część 2. Przypomnienie wiadomości
+
+        // Część 3. Stwórz nowy projekt
+    }
+}
